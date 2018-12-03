941e88fcada042da4cb28b4a7708a0d493555c78
Christopher D. Long <galizur@gmail.com>
2018-12-02 23:18:24 -0500

    Solution in SQL using Levenshtein distance.

---
+++ 2018/2/levenshtein.sql
@@ -0,0 +1,18 @@
+begin;
+
+create schema if not exists aoc;
+
+drop table if exists aoc.codes;
+
+create table aoc.codes (
+	code	      text
+);
+
+copy aoc.codes from '/tmp/input.txt' csv;
+
+select c1.code,c2.code from aoc.codes c1, aoc.codes c2
+where
+    c1.code < c2.code
+and levenshtein(c1.code,c2.code)=1;
+
+commit;
